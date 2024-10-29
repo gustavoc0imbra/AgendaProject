45ecaa3ea373412a1d8bf91c5b5c224fc240999b
Vinicius Inhesta Dos Santos <vinicius.inhesta@hotmail.com>
2024-10-29 16:05:03 -0300

    Endereco com ViaCep
    Precisa terminar de configurar para os templates e terminar a controller, MEtodo de ADD é passando somente o ID do Usuario.

--- src/main/java/org/trabalho2/agendaProject/Model/Endereco.java
+++ src/main/java/org/trabalho2/agendaProject/Model/Endereco.java
@@ -1,89 +1,29 @@
 package org.trabalho2.agendaProject.Model;
 
-import jakarta.persistence.*;
-
-import java.io.Serializable;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
+import jakarta.persistence.OneToOne;
+import lombok.*;
 
 @Entity
-public class Endereco implements Serializable {
+@Getter
+@Setter
+@ToString
+@RequiredArgsConstructor
+public class Endereco {
     @Id
     @GeneratedValue
     private Integer id;
-    
-    private String rua;
-
-    private String uf;
-
-    private String estado;
-
     private String cep;
-    private String cidade;
-    private int numero;
+    private String logradouro;
     private String complemento;
+    private Integer numero;
+    private String bairro;
+    private String localidade;
+    private String uf;
+    private String estado;
     @OneToOne
     private Cliente cliente;
 
-    public Integer getId() {
-        return id;
-    }
-
-    public void setId(Integer id) {
-        this.id = id;
-    }
-
-    public String getRua() {
-        return rua;
-    }
-
-    public void setRua(String rua) {
-        this.rua = rua;
-    }
-
-    public String getUf() {
-        return uf;
-    }
-
-    public void setUf(String uf) {
-        this.uf = uf;
-    }
-
-    public String getEstado() {
-        return estado;
-    }
-
-    public void setEstado(String estado) {
-        this.estado = estado;
-    }
-
-    public String getCep() {
-        return cep;
-    }
-
-    public void setCep(String cep) {
-        this.cep = cep;
-    }
-
-    public String getCidade() {
-        return cidade;
-    }
-
-    public void setCidade(String cidade) {
-        this.cidade = cidade;
-    }
-
-    public int getNumero() {
-        return numero;
-    }
-
-    public void setNumero(int numero) {
-        this.numero = numero;
-    }
-
-    public String getComplemento() {
-        return complemento;
-    }
-
-    public void setComplemento(String complemento) {
-        this.complemento = complemento;
-    }
 }
