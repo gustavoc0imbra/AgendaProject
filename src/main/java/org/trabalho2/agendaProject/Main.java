0e78febea8cb0d1de2fee84a2662247a0f61e740
gustavo_c0imbra <gustavocoimbra04@gmail.com>
2024-11-04 11:06:02 -0300

    Configuração bd para abrir server

--- src/main/java/org/trabalho2/agendaProject/Main.java
+++ src/main/java/org/trabalho2/agendaProject/Main.java
@@ -2,10 +2,19 @@ package org.trabalho2.agendaProject;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.h2.tools.Server;
+
+import java.sql.SQLException;
 
 @SpringBootApplication
 public class Main {
     public static void main(String[] args) {
         SpringApplication.run(Main.class, args);
+    }
+
+    @Bean(initMethod = "start", destroyMethod = "stop")
+    public Server h2Server() throws SQLException {
+        return Server.createTcpServer("-tcp", "-tcpAllowOthers", "-tcpPort", "9092");
     }
 }
